3a76d8cf4c4a986caa88b488fc49b6dce3564834
unknown <Rana Kiwan>
2024-07-29 20:36:00 +0300

    task2

--- src/Routes/tasks.js
+++ src/Routes/tasks.js
@@ -1,38 +1,50 @@
  const express = require("express");
 const router = express.Router();
-const { getTasks, createTask } = require("../services/taskServices");
+const { getTasks, createTask, updateTask, deleteTask} = require("../services/taskServices");
 
 // GET /tasks endpoint
 router.get("/tasks", (request, response) => {
-    console.log("Received a request for /tasks");
     const json = getTasks();
     return response.status(200).json(json);
 });
 
-// POST /tasks endpoint
+// POST /tasks endpoint     
 router.post("/tasks", (request, response) => {
     const body = request.body; // Extract request body
-    createTask(body); // Pass the body to the createTask service
+    const task ={title: body.title, description: body.description};
+    createTask(task); // Pass the body to the createTask service
     return response.status(201).json({
         message: "Task created"
     });
 });
 
 //updates /tasks endpoint
-router.put("/tasks", (request, response) => {
+router.put("/tasks/:id", (request, response) => {
     const body = request.body; // Extract request body
-    console.log(body.title); // Log the task title to console
-    return response.status(201).json({
+    const taskId =request.params.id
+    const task ={title: body.title, description: body.description};
+        const result =updateTask(task, taskId);
+    if (result) {
+        
+        return response.status(201).json({
         message: "Task updated"
-    });
+        
+    });}
+return response.status(404).json({message: "not found"});
 });
 
 //delete tasks
-router.delete("/tasks", (request, response) => {
-    const body = request.body; // Extract request body
-    console.log(body.title); // Log the task title to console
-    return response.status(201).json({
-        message: "Task deleted"
+    router.delete("/tasks/:id", (request, response) => {
+        const body = request.body; // Extract request body
+        const taskId =request.params.id
+            const result =deleteTask(taskId);
+        if (result) {
+            
+            return response.status(201).json({
+            message: "Task deleted"
+            
+        });}
+    return response.status(404).json({message: "not found"});
     });
-});
-module.exports = router;
+    
+    module.exports = router;
--- src/services/taskServices.js
+++ src/services/taskServices.js
@@ -1,18 +1,42 @@
-function getTasks() {
-    return {
-             title: "submit the express task 1" // Task title
-                 };
-    
+const db = require("better-sqlite3")("app.db")
+
+// function to get data from database
+function getTasks(){
+    const query = "SELECT * FROM Tasks"
+    const result = db.prepare(query).all();
+    return result;
 }
 
-function createTask() {
-        // Logging a message to indicate that a task has been created
-        console.log("Task created");
-      }
-      
-       // Exporting the getProducts and createProduct functions
-      module.exports = {
-        getTasks, // Exporting the getProducts function
-        createTask // Exporting the createTask function
-      };
-         
+// function to create data and add it to database
+function createTask(task){
+    const query = "INSERT INTO Tasks(title, description) VALUES(?, ?)"
+    const result = db.prepare(query).run(task.title, task.description)
+    // make sure that changes did occur, and specifically only 1 change
+    if(result.changes === 0){
+        throw new Error('An error occured when inserting a new product');
+    }
+}
+
+// function to update data in database
+function updateTask(task, taskId){
+    const query = " UPDATE Tasks SET title = ?, description = ? WHERE id = ? "
+    const result = db.prepare(query).run(task.title, task.description, taskId)
+    // return whether this one todo got changed successfully or not
+    return result.changes == 1;
+}
+
+// function to delete data from database
+function deleteTask(taskId){
+    const query = "DELETE FROM Tasks WHERE id = ?"
+    const result = db.prepare(query).run(taskId);
+    // rerturn whether this one todo got removed successfully or not
+    return result.changes == 1;
+}
+
+// export the functions to use in other files
+module.exports = {
+    getTasks,
+    createTask,
+    updateTask,
+    deleteTask
+}
